a88ee1e689657d60acfc6d5768696d4d5334d469
grchoi <grchoi@emro.co.kr>
2021-05-02 15:35:29 +0900

    feat : RacingCarGame main 추가

---
+++ src/main/java/RacingCarGameMain.java
@@ -0,0 +1,10 @@
+import service.RacingCarService;
+
+public class RacingCarGameMain {
+
+    public static void main(String[] args){
+        RacingCarService racingCarService = new RacingCarService();
+        racingCarService.play();
+    }
+
+}
